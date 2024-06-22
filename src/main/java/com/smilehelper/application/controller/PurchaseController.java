d4c8676feb9915ea7a14c21135fde2fbb172650c
남두현 <kndh2914@gmail.com>
2024-06-22 20:18:37 +0900

    Item, Purchase API 추가 및 yml에 코드 변경

--- src/main/java/com/smilehelper/application/controller/PurchaseController.java
+++ src/main/java/com/smilehelper/application/controller/PurchaseController.java
@@ -3,14 +3,10 @@ package com.smilehelper.application.controller;
 import com.smilehelper.application.dto.PurchaseDTO;
 import com.smilehelper.application.service.PurchaseService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/smilehelper/application/repository/PurchaseRepository.java
+++ src/main/java/com/smilehelper/application/repository/PurchaseRepository.java
@@ -3,8 +3,6 @@ package com.smilehelper.application.repository;
 import com.smilehelper.application.domain.Purchase;
 import com.smilehelper.application.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
